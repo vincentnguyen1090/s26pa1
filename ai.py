6bf008ded61e3817f74cb4475c211ada8be0ae03
VincentNguyen <vincentnguyen1090@gmail.com>
2026-04-04 16:33:26 -0700

    dfs/bfs implementation

--- ai.py
+++ ai.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 from heapq import *  # Hint: Use heappop and heappush
+from collections import deque
 
 ACTIONS = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
@@ -26,7 +27,7 @@ class AI:
             self.frontier = [self.grid.start]
             self.explored = []
         elif self.type == "bfs":
-            pass
+            self.frontier = deque([self.grid.start])
         elif self.type == "ucs":
             pass
         elif self.type == "astar":
@@ -58,12 +59,18 @@ class AI:
             self.astar_step()
 
     # TODO: Buggy DFS, fix it first
+    # Issue: we append every non-puddle child to the frontier
+    # We should not append if the child already in:
+    # 1. frontier
+    # 2. visited/explored
     def dfs_step(self):
+        # If nothing left to explore, no path found
         if not self.frontier:
             self.failed = True
             self.finished = True
             print("no path")
             return
+        # Popping last-added node from the stack
         current = self.frontier.pop()
 
         # Finishes search if we've found the goal.
@@ -71,23 +78,67 @@ class AI:
             self.finished = True
             return
 
+        # create all neighboring positions
         children = [(current[0] + a[0], current[1] + a[1]) for a in ACTIONS]
-        self.grid.nodes[current].color_checked = True
-        self.grid.nodes[current].color_frontier = False
+        self.grid.nodes[current].color_checked = True    # mark current node as explored
+        self.grid.nodes[current].color_frontier = False  # mark colored as a frontier node
 
+        # Go through every child node
         for n in children:
+            # Check if chid within boundaries
             if n[0] in range(self.grid.row_range) and n[1] in range(
                 self.grid.col_range
             ):
-                if not self.grid.nodes[n].puddle:
-                    self.previous[n] = current
-                    self.frontier.append(n)
-                    self.grid.nodes[n].color_frontier = True
+                # Skip puddles
+                if self.grid.nodes[n].puddle:
+                    continue
+                
+                # Skip nodes already explored / in frontier
+                if self.grid.nodes[n].color_checked or self.grid.nodes[n].color_frontier:
+                    continue
+                
+                self.previous[n] = current                # Keep track of parent node -> reconstuct path later
+                self.frontier.append(n)                   # Add child to frontier
+                self.grid.nodes[n].color_frontier = True  # Mark child as added to frontier
 
     # TODO: Implement BFS here (Don't forget to implement initialization in set_search function)
     def bfs_step(self):
-        self.failed = True
-        self.finished = True
+        if not self.frontier:            
+            self.failed = True
+            self.finished = True
+            print("no path")
+            return
+        # popping the first node in queue
+        current = self.frontier.popleft()
+        
+         # Finishes search if we've found the goal.
+        if current == self.grid.goal:
+            self.finished = True
+            return
+
+
+        # create all neighboring positions
+        children = [(current[0] + a[0], current[1] + a[1]) for a in ACTIONS]
+        self.grid.nodes[current].color_checked = True    # mark current node as explored
+        self.grid.nodes[current].color_frontier = False  # mark colored as a frontier node
+        
+        # Go through every child node
+        for n in children:
+            # Check if chid within boundaries
+            if n[0] in range(self.grid.row_range) and n[1] in range(
+                self.grid.col_range
+            ):
+                # Skip puddles
+                if self.grid.nodes[n].puddle:
+                    continue
+                
+                # Skip nodes already explored / in frontier
+                if self.grid.nodes[n].color_checked or self.grid.nodes[n].color_frontier:
+                    continue
+                
+                self.previous[n] = current                # Keep track of parent node -> reconstuct path later
+                self.frontier.append(n)                   # Add child to frontier
+                self.grid.nodes[n].color_frontier = True  # Mark child as added to frontier
 
     # TODO: Implement UCS here (Don't forget to implement initialization in set_search function)
     # Hint: You can use heappop and heappush from the heapq library (imported for you above)
